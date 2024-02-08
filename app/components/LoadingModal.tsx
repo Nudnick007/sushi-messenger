04166d5eee7593c58be26300bd6a2f5e018ee495
Nudnick007 <97278688+Nudnick007@users.noreply.github.com>
2024-02-08 11:01:05 +0530

    new

--- app/components/LoadingModal.tsx
+++ app/components/LoadingModal.tsx
@@ -21,7 +21,7 @@ const LoadingModal = () => {
             className="
               fixed 
               inset-0
-              bg-gray-100 
+              bg-gray-200 
               bg-opacity-50 
               transition-opacity
             "
